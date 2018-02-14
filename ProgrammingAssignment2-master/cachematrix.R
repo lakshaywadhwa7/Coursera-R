c73c6d19dc3e56d77f2b47c8cd9c3c2f938842c8
lakshaywadhwa7 <36010481+lakshaywadhwa7@users.noreply.github.com>
2018-02-14 15:17:07 +0530

    updated code

--- ProgrammingAssignment2-master/cachematrix.R
+++ ProgrammingAssignment2-master/cachematrix.R
@@ -2,12 +2,19 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## X: a square invertible matrix
+  ## return: a list with functions to
+  ##              1. set the matrix
+  ##              2. get the matrix
+  ##              3. set the inverse
+  ##              4. get the inverse
+  ##         this list is inserted intoo cacheSolve()
 makeCacheMatrix<-function(x=matrix())
 {
 inv<-NULL
 set<-function(y)
 {
+   # use `<<-` to assign a value to an object in an environment different in which we are working
 x<<-y
 inv<<-NULL
 }
@@ -21,15 +28,18 @@ getInverse=getInverse)
 
 
 ## Write a short comment describing this function
-
+## return: inverse of the original matrix and the input to makeCacheMatrix()
 cacheSolve<-function(x,...)
 {
 inv<-x$getInverse()
+ ## if the inverse exists already, is already calculated
 if(!is.null(inv))
 {
 message("getting cached data")
+  # get it from the cache and skips the calculation part for which data already existed. 
 return(inv)
 }
+ ## otherwise, calculates the inverse 
 mat<-x$get()
 inv<-solve(mat,...)
 x$setInverse(inv)
